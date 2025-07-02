10ce93dcdd376bca489e697597b0b583397755b9
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-07-02 18:44:53 +0530

    update

--- kanakproject/settings.py
+++ kanakproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--2(monv%onvl8e$bpa8dchwnxp$w5=f%vp(yam%@=uh#rc@7d=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kanakform.live', 'www.kanakform.live']
 
 
 # Application definition
